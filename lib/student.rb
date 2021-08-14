40baa97c0b61ae57be014287cce9b65467b1dd16
TenzingS <thesherpatenzing@gmail.com>
2021-08-14 17:45:40 -0400

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
 
 end
+
+sophie = Student.new
+puts sophie.learn("learning from teacher")
